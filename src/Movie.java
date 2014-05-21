d82ca316325cb317a1ef5c9a27b5faf4e3997654
Tobias Engelmann <T.O.B.B.E@web.de>
2014-05-21 13:52:15 +0200

    Move Method: get frequent renter points from movie to price

--- src/Movie.java
+++ src/Movie.java
@@ -39,9 +39,6 @@ public class Movie {
 	}
 
 	int getFrequentRenterPoints(int daysRented) {
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-			return 2;
-		else
-			return 1;
+		return this.price.getFrequentRenterPoints(daysRented);
 	}
 }
--- src/Price.java
+++ src/Price.java
@@ -2,6 +2,13 @@ public abstract class Price {
 	abstract int getPriceCode();
 
 	abstract double getCharge(int daysRented);
+
+	int getFrequentRenterPoints(int daysRented) {
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+			return 2;
+		else
+			return 1;
+	}
 }
 
 class ChildrensPrice extends Price {
