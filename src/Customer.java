77f640227ac91724a9ea9e633937d99791ce0f5c
Tobias <tobias.seber@gmail.com>
2014-05-21 12:55:58 +0200

    Extract Method: getFrequentRenterPoints() into class Rental

--- src/Customer.java
+++ src/Customer.java
@@ -27,13 +27,8 @@ class Customer {
 
 		while (enum_rentals.hasMoreElements()) {
 			Rental each = (Rental) enum_rentals.nextElement();
+			frequentRenterPoints += each.getFrequentRenterPoints();
 
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t"
 					+ each.getDaysRented() + "\t" + String.valueOf(each.getCharge())
