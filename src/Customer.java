e2aa1ec1899c2cfbd2d8798c8f0f59e309d397c5
Tobias Engelmann <T.O.B.B.E@web.de>
2014-05-21 13:05:39 +0200

    Replace temp with query: frequentRenterPoints -> own method (getTotalFrequentRenterPoints), remove old statements which are not used anymore

--- src/Customer.java
+++ src/Customer.java
@@ -18,8 +18,6 @@ class Customer {
 	};
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Enumeration enum_rentals = rentals.elements();
 		String result = "Rental Record for " + this.getName() + "\n";
 		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount"
@@ -27,17 +25,16 @@ class Customer {
 
 		while (enum_rentals.hasMoreElements()) {
 			Rental each = (Rental) enum_rentals.nextElement();
-			frequentRenterPoints += each.getFrequentRenterPoints();
 
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t"
 					+ each.getDaysRented() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
+		result += "You earned "
+				+ String.valueOf(getTotalFrequentRenterPoints())
 				+ " frequent renter points";
 		return result;
 	}
@@ -48,6 +45,16 @@ class Customer {
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
 			result += each.getCharge();
+		}
+		return result;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration rentals = this.rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getFrequentRenterPoints();
 		}
 		return result;
 	}
