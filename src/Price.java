1872b14945001a9f6f13a79d07ad3aa11a819426
Tobias Engelmann <T.O.B.B.E@web.de>
2014-05-21 13:55:46 +0200

    Replace conditional with polymorphism: getFrequentRenterPoints to subclasses if necessary

--- src/Price.java
+++ src/Price.java
@@ -4,10 +4,7 @@ public abstract class Price {
 	abstract double getCharge(int daysRented);
 
 	int getFrequentRenterPoints(int daysRented) {
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-			return 2;
-		else
-			return 1;
+		return 1;
 	}
 }
 
@@ -31,6 +28,10 @@ class NewReleasePrice extends Price {
 
 	double getCharge(int daysRented) {
 		return daysRented * 3;
+	}
+
+	int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2 : 1;
 	}
 }
 
