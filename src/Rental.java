221f8ce4b7c5754c3d949ef9357708a57b27cfd8
Tobias <tobias.seber@gmail.com>
2014-05-21 13:25:02 +0200

    Extract Method: getFrequentRenterPoints() and getCharge(int daysRented) into class Movie

--- src/Rental.java
+++ src/Rental.java
@@ -14,31 +14,12 @@ class Rental {
 	public Movie getMovie() {
 		return movie;
 	}
-
-	public double getCharge() {
-		double result = 0;
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (getDaysRented() > 2)
-				result += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (getDaysRented() > 3)
-				result += (getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
-	}
 	
-	public int getFrequentRenterPoints(){
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-			return 2;
-		else
-			return 1;
-	}
+	public double getCharge() { 
+		return movie.getCharge(daysRented); 
+	} 
+	
+	public int getFrequentRenterPoints() { 
+		return movie.getFrequentRenterPoints(daysRented); 
+	} 
 }
